d1b0f1487a1d3a1a9f70bffdfd576891d217b9ba
Huann Almeida <huannvictor@gmail.com>
2022-05-08 23:07:42 -0300

    try/catch adicionado ao routs.ts

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -6,22 +6,29 @@ import { SubmitFeedbackUseCase } from "./use-cases/submitFeedback-useCase";
 export const routes = express.Router();
 
 routes.post("/feedbacks", async (req, res) => {
-  const { type, comment, screenshot } = req.body;
 
-  const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
+  try{
+    const { type, comment, screenshot } = req.body;
+  
+    const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
+  
+    const nodemailerMailAdapter = new NodemailerMailAdapter();
+  
+    const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+      prismaFeedbacksRepository,
+      nodemailerMailAdapter,
+    );
+  
+    await submitFeedbackUseCase.execute({
+      type,
+      comment,
+      screenshot,
+    });
+  
+    return res.status(201).send();
 
-  const nodemailerMailAdapter = new NodemailerMailAdapter();
-
-  const submitFeedbackUseCase = new SubmitFeedbackUseCase(
-    prismaFeedbacksRepository,
-    nodemailerMailAdapter,
-  );
-
-  await submitFeedbackUseCase.execute({
-    type,
-    comment,
-    screenshot,
-  });
-
-  return res.status(201).send();
+  } catch (err) {
+    console.error(err)
+    return res.status(500).send()
+  }
 });
